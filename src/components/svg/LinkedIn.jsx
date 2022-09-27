22c0840d86b24ee344bd63070dd18ebcc2ab5db1
Joseph Cortez <joseph@Josephs-MacBook-Pro.local>
2022-09-26 22:15:34 -0500

    updating icons

--- src/components/svg/LinkedIn.jsx
+++ src/components/svg/LinkedIn.jsx
@@ -1,10 +1,10 @@
 export const LinkedIn = () => {
     return (
         <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect opacity="0.1" x="7.1" y="7.1" width="21.8" height="21.8" rx="3.9" fill="currentColor" stroke="currentColor" stroke-width="2.2"/>
-        <path opacity="0.57" d="M11.8143 14.2217C12.6156 14.2217 13.2653 13.6023 13.2653 12.8384C13.2653 12.0744 12.6156 11.4551 11.8143 11.4551C11.0129 11.4551 10.3633 12.0744 10.3633 12.8384C10.3633 13.6023 11.0129 14.2217 11.8143 14.2217Z" fill="currentColor"/>
-        <path opacity="0.57" d="M11.7695 16.9873V23.9038" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path opacity="0.57" d="M16.7852 16.9873V23.9038M16.7852 20.0133C16.7852 19.2107 17.1673 18.4411 17.8476 17.8736C18.5279 17.3061 19.4506 16.9873 20.4126 16.9873C21.3747 16.9873 22.2973 17.3061 22.9776 17.8736C23.6579 18.4411 24.0401 19.2107 24.0401 20.0133V23.9038" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+        <rect opacity="0.09" x="7.1" y="7.1" width="21.8" height="21.8" rx="3.9" fill="currentColor" stroke="currentColor" stroke-width="2.2"/>
+        <path opacity="0.45" d="M11.8143 14.2217C12.6156 14.2217 13.2653 13.6023 13.2653 12.8384C13.2653 12.0744 12.6156 11.4551 11.8143 11.4551C11.0129 11.4551 10.3633 12.0744 10.3633 12.8384C10.3633 13.6023 11.0129 14.2217 11.8143 14.2217Z" fill="currentColor"/>
+        <path opacity="0.45" d="M11.7695 16.9873V23.9038" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path opacity="0.45" d="M16.7852 16.9873V23.9038M16.7852 20.0133C16.7852 19.2107 17.1673 18.4411 17.8476 17.8736C18.5279 17.3061 19.4506 16.9873 20.4126 16.9873C21.3747 16.9873 22.2973 17.3061 22.9776 17.8736C23.6579 18.4411 24.0401 19.2107 24.0401 20.0133V23.9038" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     );
 }
