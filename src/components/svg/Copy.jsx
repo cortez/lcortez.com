bf0efde405ba04f255c8238e4059013c1d3c7121
Joseph Cortez <joseph@Josephs-MacBook-Pro.local>
2022-10-05 20:25:06 -0500

    updated components

--- src/components/svg/Copy.jsx
+++ src/components/svg/Copy.jsx
@@ -1,8 +1,8 @@
 export const Copy = () => {
     return (
         <svg className="copy-svg" width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path opacity="1" d="M11 13H19C21.2091 13 23 14.7909 23 17V25C23 27.2091 21.2091 29 19 29H11C8.79086 29 7 27.2091 7 25V17C7 14.7909 8.79086 13 11 13Z" stroke="currentColor" stroke-width="2.5"/>
-            <path opacity="1" d="M13 11V11C13 8.79086 14.7909 7 17 7H25C27.2091 7 29 8.79086 29 11V19C29 21.2091 27.2091 23 25 23V23" stroke="currentColor" stroke-width="2.5"/>
+            <path opacity="1" d="M11 13H19C21.2091 13 23 14.7909 23 17V25C23 27.2091 21.2091 29 19 29H11C8.79086 29 7 27.2091 7 25V17C7 14.7909 8.79086 13 11 13Z" stroke="currentColor" stroke-width="2.3"/>
+            <path opacity="1" d="M13 11V11C13 8.79086 14.7909 7 17 7H25C27.2091 7 29 8.79086 29 11V19C29 21.2091 27.2091 23 25 23V23" stroke="currentColor" stroke-width="2.3"/>
         </svg>
     );
 }
