ebc7a16d1894dbc0c2d681a533b42147c34ea76f
Joseph Cortez <joseph@Josephs-MacBook-Pro.local>
2022-12-01 23:25:39 -0600

    Committed with Git-Upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className="render">
           <span className="caps" data-aos="zoom-in" data-aos-delay="350">Joseph Cortez</span>
-          <p data-aos="zoom-in" data-aos-delay="300">Recent computer information systems graduate interested in building compelling brand identities and intuitive software. </p>
+          <p data-aos="zoom-in" data-aos-delay="300">Recent computer information systems graduate and software developer.</p>
           <br /><br />
           <span className="caps" data-aos="zoom-in" data-aos-delay="350">Selected Projects</span>
           <br />
